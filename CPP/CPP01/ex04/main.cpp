a1d148bfbf906641ad4899df07480cdd6a510e13
sehwjang <sehwan1023@gmail.com>
2024-08-23 10:28:37 +0900

    CPP01 V3

--- CPP/CPP01/ex04/main.cpp
+++ CPP/CPP01/ex04/main.cpp
@@ -1,55 +1,41 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sehwjang <sehwjang@student.42seoul.kr>     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/06 20:44:34 by sehwjang          #+#    #+#             */
-/*   Updated: 2024/08/09 18:11:50 by sehwjang         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 
 void replaceStringInFile(const std::string& filename, const std::string& s1, const std::string& s2) {
     std::ifstream inputFile(filename);
-    if (!inputFile) {
+    if (!inputFile.is_open()) {
         std::cerr << "Error: Could not open the input file." << std::endl;
         return;
     }
 
+    std::ostringstream ss;
+    ss << inputFile.rdbuf();
+    std::string fileContents = ss.str();
+    inputFile.close();
+
     std::string outputFilename = filename + ".replace";
     std::ofstream outputFile(outputFilename);
-    if (!outputFile) {
+    if (!outputFile.is_open()) {
         std::cerr << "Error: Could not open/create the output file." << std::endl;
         return;
     }
-
-    std::string line;
-    while (std::getline(inputFile, line)) {
-		std::string result;
-        size_t pos = 0;
-        while ((pos = line.find(s1, pos)) != std::string::npos) {
-            result.append(line, 0, pos);
-            result.append(s2);
-            line = line.substr(pos + s1.length());
-            pos = 0;
-        }
-        result.append(line);
-        outputFile << result << std::endl;
+    size_t pos = 0;
+    while ((pos = fileContents.find(s1, pos)) != std::string::npos) {
+        fileContents.replace(pos, s1.length(), s2);
+        pos += s2.length();
     }
-	
-    inputFile.close();
+
+    outputFile << fileContents;
     outputFile.close();
 }
 
-int main(int argc, char *argv[]){
-	if (argc != 4){
-		std::cout<<"Wrong input!"<<std::endl;
-		return (1);
-	}
-	replaceStringInFile(argv[1], argv[2], argv[3]);
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <filename> <search_string> <replace_string>" << std::endl;
+        return 1;
+    }
+    replaceStringInFile(argv[1], argv[2], argv[3]);
+    return 0;
 }
