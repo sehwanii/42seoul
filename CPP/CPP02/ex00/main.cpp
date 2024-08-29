251a63977fe29dd1a6717fdc9395a731926e23d5
sehwjang <sehwan1023@gmail.com>
2024-08-29 17:33:56 +0900

    CPP02 v1

--- CPP/CPP02/ex00/main.cpp
+++ CPP/CPP02/ex00/main.cpp
@@ -6,20 +6,21 @@
 /*   By: sehwjang <sehwjang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 18:47:49 by sehwjang          #+#    #+#             */
-/*   Updated: 2024/08/09 18:48:15 by sehwjang         ###   ########.fr       */
+/*   Updated: 2024/08/29 17:26:46 by sehwjang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void ) {
-Fixed a;
-Fixed b( a );
-Fixed c;
-c = b;
-std::cout << a.getRawBits() << std::endl;
-std::cout << b.getRawBits() << std::endl;
-std::cout << c.getRawBits() << std::endl;
-return 0;
+int main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
 }
