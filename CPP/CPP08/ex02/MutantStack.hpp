ab66423b3144d1a43978c71a3839c6bf7bafd20c
sehwanii_mac <sehwan1023@gmail.com>
2025-06-09 14:48:17 +0900

    cpp08

--- CPP/CPP08/ex02/MutantStack.hpp
+++ CPP/CPP08/ex02/MutantStack.hpp
@@ -1,33 +1,32 @@
-#ifndef MUTANTSTACK_HPP
-#define MUTANTSTACK_HPP
+#pragma once
 
 #include <stack>
+#include <deque>
 
 template <typename T>
 class MutantStack : public std::stack<T> {
-public:
-    MutantStack() {}
-    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
-    MutantStack& operator=(const MutantStack& other) {
-        std::stack<T>::operator=(other);
-        return *this;
-    }
-    ~MutantStack() {}
+	public:
+		MutantStack(void) {};
+		MutantStack(const MutantStack& obj) {
+			*this = obj;
+		};
+		MutantStack& operator=(const MutantStack& obj) {
+			*this = obj; return (*this);
+		};
+		~MutantStack(void) {};
 
-    typedef typename std::stack<T>::container_type::iterator iterator;
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-
-    iterator begin() { return this->c.begin(); }
-    iterator end() { return this->c.end(); }
-    const_iterator begin() const { return this->c.begin(); }
-    const_iterator end() const { return this->c.end(); }
-
-    reverse_iterator rbegin() { return this->c.rbegin(); }
-    reverse_iterator rend() { return this->c.rend(); }
-    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
-    const_reverse_iterator rend() const { return this->c.rend(); }
+		typedef typename MutantStack<T>::stack::container_type::iterator iterator;
+		iterator begin(void) {
+			return this->c.begin();
+		}
+		iterator end(void) {
+			return this->c.end();
+		}
+		typedef typename MutantStack<T>::stack::container_type::const_iterator const_iterator;
+		const_iterator begin(void) const {
+			return this->c.begin();
+		}
+		const_iterator end(void) const {
+			return this->c.end();
+		}
 };
-
-#endif
--- CPP/CPP08/ex02/main.cpp
+++ CPP/CPP08/ex02/main.cpp
@@ -1,31 +1,68 @@
 #include <iostream>
+#include <list>
 #include "MutantStack.hpp"
 
-int main() {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << "TOP: "<<mstack.top() << std::endl;
+int main( void ) {
+	std::cout << "\033[32;7m" << "1. MutantStack " << "\033[0m" << std::endl;
+	MutantStack<int> mstack;
 
-    mstack.pop();
-    std::cout << "SIZE: "<< mstack.size() << std::endl;
+	mstack.push(5);
+	mstack.push(17);
+	
+	std::cout << "\033[30;2m" << "//top()" << "\033[0m" << std::endl;
+	std::cout << mstack.top() << std::endl;
+	
+	std::cout << "\033[30;2m" << "//void pop()" << "\033[0m" << std::endl;
+	mstack.pop();
+	
+	std::cout << "\033[30;2m" << "//size()" << "\033[0m" << std::endl;
+	std::cout << mstack.size() << std::endl;
+	
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...] 
+	mstack.push(0);
+	
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	
+	++it;
+	--it;
+	
+	std::cout << "\033[30;2m" << "//while() {}" << "\033[0m" << std::endl;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
 
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
 
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
 
-    ++it;
-    --it;
 
-    while (it != ite) {
-        std::cout << *it << std::endl;
-        ++it;
-    }
-
-    std::stack<int> s(mstack);
-    return 0;
+	std::cout << "\033[32;7m" << "\n2. std::list " << "\033[0m" << std::endl;
+	std::list<int> list;
+	list.push_back(5);
+	list.push_back(17);
+	std::cout << "\033[30;2m" << "//back()" << "\033[0m" << std::endl;
+	std::cout << list.back() << std::endl;
+	std::cout << "\033[30;2m" << "//void pop()" << "\033[0m" << std::endl;
+	list.pop_back();
+	std::cout << "\033[30;2m" << "//size()" << "\033[0m" << std::endl;
+	std::cout << list.size() << std::endl;
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	//[...] 
+	list.push_back(0);
+	std::list<int>::iterator it2 = list.begin();
+	std::list<int>::iterator ite2 = list.end();
+	++it2;
+	--it2;
+	std::cout << "\033[30;2m" << "//while() {}" << "\033[0m" << std::endl;
+	while (it2 != ite2) {
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	return 0;
 }
